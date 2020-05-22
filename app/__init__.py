324f27e9cf9aa3a8f8bf7d004cda89d432e22ef7
Andreas Gropp <starmaerker@googlemail.com>
2020-05-22 22:31:12 +0200

    add base.html and index.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html', var="World!")
