da97d9bca09c35255a40d454af984ec504b82dad
Andreas Gropp <starmaerker@googlemail.com>
2020-05-26 22:25:51 +0200

    add login route

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/index')
@@ -7,3 +8,9 @@ from app import app
 def hello_world():
     member = ['Christine Rasche', 'Rubens Romanello', 'Viet-Hung Dinh', 'Andreas Gropp']
     return render_template('index.html', member=member)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
