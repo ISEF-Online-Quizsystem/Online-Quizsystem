3980f1b2334a2cbc47f482b4e10d5e933f91c70f
Andreas Gropp <starmaerker@googlemail.com>
2020-05-27 16:47:09 +0200

    add flask-sqlalchemy

--- app/routes.py
+++ app/routes.py
@@ -15,5 +15,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Anmeldung', form=form)
