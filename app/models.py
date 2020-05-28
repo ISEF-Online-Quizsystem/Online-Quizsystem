5f05a953b8d09683a5be7eb87e543f2fa24d456c
Andreas Gropp <starmaerker@googlemail.com>
2020-05-28 15:27:07 +0200

    update models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 
 class User(db.Model):
@@ -9,3 +10,24 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    module = db.Column(db.String(64), index=True)
+    question = db.Column(db.String(280))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    answers = db.relationship('Answer', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<Question {self.question}>'
+
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    option_one = db.Column(db.String(64))
+    option_two = db.Column(db.String(64))
+    option_three = db.Column(db.String(64))
+    option_four = db.Column(db.String(64))
+    option_five = db.Column(db.String(64))
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
