0a6a5855365373f205578af7c53fda924681a575
Andreas Gropp <starmaerker@googlemail.com>
2020-05-28 16:03:53 +0200

    add shell context to application.py

--- application.py
+++ application.py
@@ -1,1 +1,8 @@
-from app import app
+from app import app, db
+from app.models import User, Question, Answer
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Question': Question, 'Answer': Answer}
+
