fc0e7f13343adb3911c4e81e189f4d90dc9c5cd9
Andreas Gropp <starmaerker@googlemail.com>
2020-05-29 15:23:44 +0200

    update login route

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/index')
 @app.route('/')
+@login_required
 def index():
 
     return render_template('index.html')
@@ -23,11 +25,14 @@ def login():
             flash('Ungültiger Benutzername oder Passwort.')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Anmeldung', form=form)
 
 
-@app.rout('/logout')
+@app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
