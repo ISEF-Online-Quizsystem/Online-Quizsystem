9a613e0521cf16c52dbc18849e2b823d285e11a9
Andreas Gropp <starmaerker@googlemail.com>
2020-05-30 22:06:20 +0200

    add fields to class User

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,14 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route('/index')
