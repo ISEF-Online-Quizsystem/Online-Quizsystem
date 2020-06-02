c207799e83fea3aeff4df18d826d502a1a66291f
Andreas Gropp <starmaerker@googlemail.com>
2020-06-02 22:22:27 +0200

    fix edit profile bug

--- app/forms.py
+++ app/forms.py
@@ -34,6 +34,16 @@ class EditProfileForm(FlaskForm):
     about_me = TextAreaField('Über mich', validators=[Length(min=0, max=140)])
     submit = SubmitField('Abschicken')
 
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Bitte benutze einen anderen Benutzernamen.')
+
 
 class ResetPasswordRequestForm(FlaskForm):
     email = StringField('E-Mail', validators=[DataRequired(), Email()])
