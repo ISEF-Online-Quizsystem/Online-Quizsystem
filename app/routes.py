87863acd8804313dba6dbd24f5c0f386b1444a2e
Andreas Gropp <starmaerker@googlemail.com>
2020-06-03 16:32:46 +0200

    fix bugs password reset

--- app/routes.py
+++ app/routes.py
@@ -90,7 +90,7 @@ def edit_profile():
 def reset_password_request():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    form = ResetPasswordRequestForm
+    form = ResetPasswordRequestForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user:
