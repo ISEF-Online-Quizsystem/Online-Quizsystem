5ce451f6e5d9752745d9fad800f1cd50903f6afa
Andreas Gropp <starmaerker@googlemail.com>
2020-06-04 07:32:11 +0200

    fix bug in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -18,14 +18,14 @@ login.login_view = 'login'
 mail = Mail(app)
 
 
-if not app.debug and not app.testing:
+if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
         secure = None
         if app.config['MAIL_USE_TLS']:
-            secure()
+            secure = ()
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
