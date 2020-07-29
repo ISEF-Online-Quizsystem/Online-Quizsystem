874b3bf442703c00f373d78e1e360143234c51b5
Andreas Gropp <starmaerker@googlemail.com>
2020-07-29 22:35:01 +0200

    update config.py

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ class Config(object):
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = os.environ.get('ADMINS') or ['iubhquiz@gmail.com']
